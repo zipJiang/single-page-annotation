821266b2b2a26fe3236abd83cf8b77585b836c6e
Chandler May <chandler.camille.may@gmail.com>
2025-01-23 23:18:03 +0000

    WIP

--- src/prototypes/SelectableTextBlock.jsx
+++ src/prototypes/SelectableTextBlock.jsx
@@ -1,12 +1,11 @@
 import "./SelectableTextBlock.css";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/system";
-import { stepContentClasses, Typography } from "@mui/material";
-import { NoEncryption } from "@mui/icons-material";
+import { Typography } from "@mui/material";
 
-function tokenSpanIsSet(tokenSpan) {
+function tokenSpanIsNonEmpty(tokenSpan) {
   return (
     tokenSpan &&
     tokenSpan.length === 2 &&
@@ -14,9 +13,10 @@ function tokenSpanIsSet(tokenSpan) {
     tokenSpan[1] !== -1
   )
 }
+
 function tokenIsInSpan(tokenIndex, tokenSpan) {
   return (
-    tokenSpanIsSet(tokenSpan) &&
+    tokenSpanIsNonEmpty(tokenSpan) &&
     tokenSpan[0] <= tokenIndex &&
     tokenSpan[1] >= tokenIndex
   );
@@ -35,10 +35,10 @@ function tokenIsInSpanAfterStart(tokenIndex, tokenSpan) {
 }
 
 function SelectableTextBlock(props) {
-
     const {
         tokens,
         onSelect,
+        prefix = null,
         selectedTokenSpan = null,
         bColor = null,
         variant = "body1",
@@ -46,214 +46,144 @@ function SelectableTextBlock(props) {
         disabled = false,
     } = props;
 
-    const [dragStartTokenIndex, setDragStartTokenIndex] = useState(-1);
-    const [dragLastTokenIndex, setDragLastTokenIndex] = useState(-1);
+    const dragStartTokenIndex = useRef(-1);
+    const dragLastTokenIndex = useRef(-1);
     const targetRef = useRef(null);
 
-    useEffect(() => {
-        if (targetRef.current) {
-            // only scroll if parentRef is scrollable
-            const isScrollable = parentRef && parentRef.current.scrollHeight > parentRef.current.clientHeight;
-            if (isScrollable) {
-                targetRef.current.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "nearest" });
-            };
-        }
-    }, [tokens, selectedTokenSpan]);
+    const spansToHighlight = tokenSpanIsNonEmpty(selectedTokenSpan)
+      ? [selectedTokenSpan]
+      : [];
 
-    const Highlight = styled("span")(({ theme }) => ({
-      display: 'inline-block',
-      userSelect: 'none',
-      backgroundColor: bColor,
-      color: theme.palette.text.primary,
-    }));
+    const firstHighlightedTokenIndex = tokens.findIndex((_, tokenIndex) =>
+      spansToHighlight.some((span) => tokenIsInSpan(tokenIndex, span))
+    );
 
-    const Text = styled("span")(({ theme }) => ({
-      display: 'inline-block',
-      userSelect: 'none',
-      color: theme.palette.text.primary,
-    }));
-
-    const HighlightText = ({ text, selection }) => {
-        if (!selection || !text.includes(selection)) {
-            return <Typography variant={variant} component={"div"}>
-                <b>{prefix}</b>
-                {text}
-            </Typography>;
-        }
-      
-        // Split the text into three parts: before, the match, and after
-        const parts = text.split(selection);
-      
-        return (
-            <Typography variant={variant} component={"div"}>
-                <b>{prefix}</b>
-                {parts[0]}
-                <Highlight 
-                    ref={targetRef}
-                    id="highlighted-review"
-                >{selection}</Highlight>
-                {parts[1]}
-            </Typography>
-        );
-    };
-
-    function spansToHighlight() {
-      return tokenSpanIsSet(selectedTokenSpan)
-        ? [selectedTokenSpan]
-        : [];
+    function tokenIsHighlighted(tokenIndex) {
+      return spansToHighlight.some((span) => tokenIsInSpan(tokenIndex, span));
     }
 
-    function tokenIsHighlighted() {
-      return tokens.map((_, tokenIndex) =>
-        spansToHighlight().some((span) => tokenIsInSpan(tokenIndex, span))
-      );
-    }
-
-    function leadingTextIsHighlighted() {
-      return tokens.map((_, tokenIndex) =>
-        spansToHighlight().some((span) =>
-          tokenIsInSpanAfterStart(tokenIndex, span)
-        )
-      );
+    function leadingTextIsHighlighted(tokenIndex) {
+      return spansToHighlight.some((span) =>
+        tokenIsInSpanAfterStart(tokenIndex, span)
+      )
     }
 
     function selectedText() {
-      return tokenSpanIsSet(selectedTokenSpan)
+      return tokenSpanIsNonEmpty(selectedTokenSpan)
         ? tokens
             .slice(selectedTokenSpan[0], selectedTokenSpan[1] + 1)
             .join(" ")
         : null;
     }
 
-    function textClass(isHighlighted) {
-      return isHighlighted ? 'highlighted' : '';
-    }
-
-    function tokenClass(tokenIndex) {
-      const isHighlighted = tokenIsHighlighted()[tokenIndex].length;
-      return textClass(selectedCount);
-    }
-
-    function leadingTextClass(tokenIndex) {
-      const isHighlighted = leadingTextIsHighlighted()[tokenIndex].length;
-      return textClass(selectedCount);
-    }
-
-    function setDrag(startTokenIndex, lastTokenIndex) {
-      setDragStartTokenIndex(startTokenIndex);
-      setDragLastTokenIndex(lastTokenIndex);
-    }
-
-    function resetDrag() {
-      setDrag(-1, -1);
-    }
-
-    function updateDrag(lastTokenIndex) {
-      setDragLastTokenIndex(lastTokenIndex);
+    function dragInProgress() {
+      return (
+        tokenSpanIsNonEmpty(selectedTokenSpan)
+        && dragStartTokenIndex.current !== -1
+        && dragLastTokenIndex.current !== -1
+      );
     }
 
     function onMouseEnter(tokenIndex) {
-      if (
-        tokenSpanIsSet(selectedTokenSpan) &&
-        dragStartTokenIndex !== -1 &&
-        dragLastTokenIndex !== -1
-      ) {
-        if (tokenIndex != dragLastTokenIndex) {
+      if (dragInProgress()) {
+        if (tokenIndex != dragLastTokenIndex.current) {
+          dragLastTokenIndex.current = tokenIndex;
           onSelect({
             span: [
-              Math.min(dragStartTokenIndex, tokenIndex),
-              Math.max(dragStartTokenIndex, tokenIndex)
+              Math.min(dragStartTokenIndex.current, tokenIndex),
+              Math.max(dragStartTokenIndex.current, tokenIndex)
             ],
             text: selectedText(),
           });
         }
-        updateDrag(tokenIndex);
       } else {
-        resetDrag();
+        dragStartTokenIndex.current = -1;
+        dragLastTokenIndex.current = -1;
       }
     }
 
     function onMouseDown(tokenIndex) {
-      setDrag(tokenIndex, tokenIndex);
+      dragStartTokenIndex.current = tokenIndex;
+      dragLastTokenIndex.current = tokenIndex;
       onSelect([tokenIndex, tokenIndex]);
     }
 
     function onMouseLeave() {
-      if (
-        !tokenSpanIsSet(selectedTokenSpan) ||
-        dragStartTokenIndex === -1 ||
-        dragLastTokenIndex === -1
-      ) {
-        resetDrag();
+      if (!dragInProgress()) {
+        dragStartTokenIndex.current = -1;
+        dragLastTokenIndex.current = -1;
       }
     }
 
+    const TextSpan = styled("span")(() => {
+      const baseStyle = {
+        display: 'inline-block',
+        userSelect: 'none',
+        color: theme.palette.text.primary,
+      };
+      return disabled
+        ? {...baseStyle, pointerEvents: 'none'}
+        : baseStyle;
+    });
+
     const LeadingText = ({ tokenIndex, text }) => {
-      const escapedText = text.replaceAll(" ", "&nbsp;");
-      return leadingTextIsHighlighted()[tokenIndex]
-        ? <Highlight>{escapedText}</Highlight>
-        : <Text>{escapedText}</Text>;
-      return (
-        <span
-        class={
-          'd-inline-block user-select-none pe-none' + (
-            disabled
-              ? ''
-              : 'pe-none'
-          ) + leadingTextClass(tokenIndex)
-        }
-        >{ text.replaceAll(" ", "&nbsp;") }
-        </span>
-      );
+      return <TextSpan
+        sx={leadingTextIsHighlighted(tokenIndex) ? {bgColor: bColor} : {}}
+      >
+        {text.replaceAll(" ", "&nbsp;")}
+      </TextSpan>;
     };
 
     const Token = ({ index, text }) => {
-      const escapedText = text.replaceAll(" ", "&nbsp;");
-      return tokenIsHighlighted()[index]
-        ? <Highlight             onMouseEnter={() => onMouseEnter(index)}
+      return <TextSpan
+        sx={tokenIsHighlighted(tokenIndex) ? {bgColor: bColor} : {}}
+        ref={index === firstHighlightedTokenIndex ? targetRef : null}
+        onMouseEnter={() => onMouseEnter(index)}
         onMouseDown={() => onMouseDown(index)}
-        onMouseLeave={onMouseLeave}>{escapedText}</Highlight>
-        : <Text             onMouseEnter={() => onMouseEnter(index)}
-        onMouseDown={() => onMouseDown(index)}
-        onMouseLeave={onMouseLeave}>{escapedText}</Text>;
-      return (
-          <span
-            class={
-              'd-inline-block user-select-none position-relative' + (
-                disabled
-                  ? 'selectable-token'
-                  : 'pe-none'
-              ) + tokenClass(index)
-            }
-            onMouseEnter={() => onMouseEnter(index)}
-            onMouseDown={() => onMouseDown(index)}
-            onMouseLeave={onMouseLeave}
-            >{ text.replaceAll(" ", "&nbsp;") }
-          </span>
-      );
+        onMouseLeave={onMouseLeave}
+      >
+        {text.replaceAll(" ", "&nbsp;")}
+      </TextSpan>;
     };
 
     useEffect(() => {
-      function onMouseUp() {
-        resetDrag();
+      if (targetRef.current) {
+        // only scroll if parentRef is scrollable
+        const isScrollable = (
+          parentRef
+          && parentRef.current
+          && parentRef.current.scrollHeight > parentRef.current.clientHeight
+        );
+        if (isScrollable) {
+          targetRef.current.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "nearest" });
+        };
       }
-      document.addEventListener('mouseup', onMouseUp);
+    }, [parentRef]);  // (Almost) empty dependencies: call just once, after initial render
+
+    useEffect(() => {
+      function resetDrag() {
+        dragStartTokenIndex.current = -1;
+        dragLastTokenIndex.current = -1;
+      }
+      document.addEventListener('mouseup', resetDrag);
       return () => {
-        document.removeEventListener('mouseup', onMouseUp);
+        document.removeEventListener('mouseup', resetDrag);
       };
-    }, [resetDrag]);
+    }, []);  // Empty dependencies: call just once, after initial render
 
     return (
       <Box>
-        {
-          tokens.flatMap((token, tokenIndex) =>
-            (
-              tokenIndex >= 0
-                ? [<LeadingText tokenIndex={tokenIndex} text=" " />]
-                : []
-            ).concat([<Token index={tokenIndex} text={token} />])
-          )
-        }
+        <Typography variant={variant} component={"div"}>
+          {prefix ? <b>{prefix}</b> : null}
+          {
+            tokens.flatMap((token, tokenIndex) => {
+              const tokenElement = <Token index={tokenIndex} text={token} />;
+              return tokenIndex >= 0
+                ? [<LeadingText tokenIndex={tokenIndex} text=" " />, tokenElement]
+                : [tokenElement];
+            })
+          }
+        </Typography>
       </Box>
     );
 }
